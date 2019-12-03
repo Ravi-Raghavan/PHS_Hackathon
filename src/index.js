583100e88301b47284f2d39729ef6d192069a998
Ravi Raghavan <raviraghavan@Ravis-Air.home>
2019-12-03 16:39:49 -0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '/Users/raviraghavan/phs-project/src/index.css';
-import Main from '/Users/raviraghavan/phs-project/src/Main/Main.js';
+//import '/Users/raviraghavan/phs-project/src/index.css';
+//import Main from '/Users/raviraghavan/phs-project/src/Main/Main.js';
 import * as serviceWorker from './serviceWorker';
-import Shelter from '/Users/raviraghavan/phs-project/src/Shelter/Shelter.js'
-import Donate from '/Users/raviraghavan/phs-project/src/Donate/Donate.js'
-import App from '/Users/raviraghavan/phs-project/src/App/App.js'
+//import Shelter from '/Users/raviraghavan/phs-project/src/Shelter/Shelter.js'
+//import Donate from '/Users/raviraghavan/phs-project/src/Donate/Donate.js'
+//import App from '/Users/raviraghavan/phs-project/src/App/App.js'
 ReactDOM.render(<App/>, document.getElementById('main'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
